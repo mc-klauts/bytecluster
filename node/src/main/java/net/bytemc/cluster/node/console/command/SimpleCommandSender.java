35eda31a126f04fa6813f02cc0aec001cb5424f6
officialMax <maxipongratz64@gmail.com>
2023-06-12 11:33:18 +0200

    update using internal cloud logger instead of console printstream

--- node/src/main/java/net/bytemc/cluster/node/console/command/SimpleCommandSender.java
+++ node/src/main/java/net/bytemc/cluster/node/console/command/SimpleCommandSender.java
@@ -1,11 +1,12 @@
 package net.bytemc.cluster.node.console.command;
 
 import net.bytemc.cluster.api.command.interfaces.CommandSender;
+import net.bytemc.cluster.node.logger.Logger;
 
 public final class SimpleCommandSender implements CommandSender {
 
     @Override
     public void sendMessage(String text) {
-        System.out.println(text);
+        Logger.info(text);
     }
 }
