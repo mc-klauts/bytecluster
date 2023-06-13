e09c3341c58d3bff29edc5d1d2674e276cf08f50
HttxMarco <mircolindenau04@gmail.com>
2023-06-13 08:37:40 +0200

    Implement check, if channel present

--- node/src/main/java/net/bytemc/cluster/node/services/LocalCloudService.java
+++ node/src/main/java/net/bytemc/cluster/node/services/LocalCloudService.java
@@ -4,6 +4,7 @@ import io.netty5.channel.Channel;
 import lombok.Getter;
 import lombok.Setter;
 import net.bytemc.cluster.api.Cluster;
+import net.bytemc.cluster.api.logging.Logger;
 import net.bytemc.cluster.api.network.Packet;
 import net.bytemc.cluster.api.service.AbstractCloudService;
 import net.bytemc.cluster.api.service.CloudServiceState;
@@ -18,14 +19,17 @@ import java.nio.file.Path;
 @Getter
 public final class LocalCloudService extends AbstractCloudService {
 
-    @Nullable @Setter
+    @Nullable
+    @Setter
     private Process process;
 
     // duplicate this entry, because not allow to modify the original api source code
-    @Getter @Setter
+    @Getter
+    @Setter
     private CloudServiceState state = CloudServiceState.OPEN;
 
-    @Nullable @Setter
+    @Nullable
+    @Setter
     private Channel channel;
 
     public LocalCloudService(String hostname, String groupName, String motd, int port, int id, int maxPlayers) {
@@ -55,7 +59,7 @@ public final class LocalCloudService extends AbstractCloudService {
     public void shutdown() {
         Cluster.getInstance().getServiceProvider().getFactory().stop(this);
     }
-    
+
     public @NotNull Path getDirectory() {
         return Node.getInstance().getRuntimeConfiguration().getNodePath().getServerRunningPath().resolve(getName());
     }
@@ -66,6 +70,10 @@ public final class LocalCloudService extends AbstractCloudService {
     }
 
     public void sendPacket(Packet packet) {
-        this.channel.writeAndFlush(packet);
+        if (this.channel != null && this.channel.isActive()) {
+            this.channel.writeAndFlush(packet);
+        } else {
+            Logger.warn("Try to send " + packet.getClass().getSimpleName() + " to " + getName() + ", but the channel is not active.");
+        }
     }
 }
