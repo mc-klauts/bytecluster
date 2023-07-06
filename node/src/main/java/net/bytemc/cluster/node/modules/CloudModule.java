7bd62b4f5b4817a366817c1bb6c16016ff3068de
HttpMarco <mirco.lindenau@gmx.de>
2023-07-06 23:26:03 +0200

    Better properties

--- node/src/main/java/net/bytemc/cluster/node/modules/CloudModule.java
+++ node/src/main/java/net/bytemc/cluster/node/modules/CloudModule.java
@@ -2,6 +2,10 @@ package net.bytemc.cluster.node.modules;
 
 import net.bytemc.cluster.api.misc.FileHelper;
 import net.bytemc.cluster.node.Node;
+import net.bytemc.cluster.node.configuration.ConfigurationHelper;
+
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public interface CloudModule {
@@ -18,5 +22,22 @@ public interface CloudModule {
         FileHelper.createDirectoryIfNotExists(path);
 
         return path;
+    }
+
+    default <T> T setConfiguration(Path path, T value) {
+        return ConfigurationHelper.readConfiguration(getConfigurationPath().resolve(path), value);
+    }
+
+    default void removeConfiguration(Path path) {
+        try {
+            Files.deleteIfExists(getConfigurationPath().resolve(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    default void updateConfiguration(Path path, Object value) {
+        this.removeConfiguration(path);
+        this.setConfiguration(path, value);
     }
 }
